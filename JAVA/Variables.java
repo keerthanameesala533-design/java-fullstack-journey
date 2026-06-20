79a67a2a8d80bffef95172212b24b3ea46db1f61
Keerthana <https://github.com/keerthanameesala533-design>
2026-06-20 19:57:29 +0530

    Add file

---
+++ JAVA/Variables.java
@@ -0,0 +1,14 @@
+class Variables {
+    public static void main(String[] args) {
+        int a = 30;
+        String name = "Keerthi";
+        char grade = 'A';
+        boolean isStudent = true;
+        double b = 60.00;
+        System.out.println("   " + a);
+        System.out.println("  " + name);
+        System.out.println("  " + grade);
+        System.out.println("  " + isStudent);
+        System.out.println("  " + b);
+    }
+}
