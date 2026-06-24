e4d13511185458686e7a48e40d140c9667092ced
Keerthana <https://github.com/keerthanameesala533-design>
2026-06-24 19:10:49 +0530

    Add file

---
+++ JAVA/TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double num = 25.75;
+        int value = (int) num; // Explicit type casting
+
+        System.out.println("Double value: " + num);
+        System.out.println("Integer value: " + value);
+    }
+}
---
+++ JAVA/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        int num = 25;
+        double value = num; // int is automatically converted to double
+
+        System.out.println("Integer value: " + num);
+        System.out.println("Double value: " + value);
+    }
+}
